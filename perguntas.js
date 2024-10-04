57705ab8feff373880aad326dd2cad238378bfe5
pedroviudes23 <142232591+pedroviudes23@users.noreply.github.com>
2024-10-04 11:34:14 -0300

    trabalho
    trabalho

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'futebol',
+    'quantos gol cr7 (cristiano ronaldo) tem na carreira?',
+    '904'
+)
+
+criaCartao(
+    'futebol',
+    'qual e o nome do zagueiro mais cobiçado do milan ?',
+    'paolo maldini'
+)
+
+criaCartao(
+    'futebol',
+    'quem foi mo jogador mais avassalador em campo?',
+    'pele'
+)
+
+criaCartao(
+    'futebol',
+    'quem foi considerado o melhor jogador em campo na copa de 2002',
+    'ronaldo fenomeno'
+)
